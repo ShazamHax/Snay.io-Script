2badfab7c8163b62cad3b077dddde66d6c399b87
ShazamHax <116787686+ShazamHax@users.noreply.github.com>
2023-06-05 17:09:13 +0800

    Update ScriptCode.js

--- ScriptCode.js
+++ ScriptCode.js
@@ -2,7 +2,7 @@
     'use strict';
 	var myPatreon = document.createElement("div");
 //myPatreon.style = "height: 50px; width: 120px; position: absolute; background-image: url('https://cdn.dribbble.com/users/2287419/screenshots/15177508/media/685ba889bceaec17a7742495ff1a4f92.gif'); border-radius: 25px; border: 2px solid blue";
-myPatreon.style = "height: 50px; width: 120px; position: absolute; border-radius: 25px; border: 2px solid blue; background: gray; text-align: center;";
+myPatreon.style = "height: 50px; width: 120px; position: absolute; border-radius: 25px; border: 2px solid blue; background: gray; text-align: center; color: white;";
 myPatreon.innerText = "Patreon";
 document.querySelector("#main-menu").append(myPatreon)
 
@@ -16,7 +16,7 @@ myPatreon.onclick = ()=>{
 myPatreon.style.cursor = "pointer";
 	var myDiscord = document.createElement("div");
 //myDiscord.style = "height: 50px; width: 120px; position: absolute; top: -15px; right: 5px; background-image: url('https://cdn.dribbble.com/users/5242374/screenshots/16641455/media/0a74ea6b1d505b316ced8be139175fc3.gif'); border: 2px solid blue; border-radius: 25px;";
-myDiscord.style = "height: 50px; width: 120px; position: absolute; top: -15px; right: 5px; border: 2px solid blue; border-radius: 25px; background: gray; text-align: center;";
+myDiscord.style = "height: 50px; width: 120px; position: absolute; top: -15px; right: 5px; border: 2px solid blue; border-radius: 25px; background: gray; text-align: center; color: white;";
 myDiscord.innerText = "Discord";
     document.querySelector("#main-menu").append(myDiscord);
     myDiscord.onclick = ()=>{
